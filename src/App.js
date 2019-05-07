5ffddea921ddad6b89c683b94106a60900198646
Diego Rocha <diego@netpos.com.br>
2019-05-07 16:10:20 -0300

    Fix array

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
   const [viewMode, setViewMode] = useState('GRID');
   const [categoryFilter, setCategoryFilter] = useState(-1);
   const [order, setOrder] = useState('AZ');
-  const [store, setStore] = useState([]);
+  const [store, setStore] = useState({});
 
   useEffect(() => {
     getStoreInfo(process.env.REACT_APP_STORE).then(response => setStore(response.data));
