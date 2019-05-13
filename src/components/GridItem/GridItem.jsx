5e50f122c7c42d3d5f0aa06dda33d5677b850a27
Diego Rocha <diego@netpos.com.br>
2019-05-13 13:07:20 -0300

    Fix grid mobile

--- src/components/GridItem/GridItem.jsx
+++ src/components/GridItem/GridItem.jsx
@@ -14,29 +14,13 @@ const Item = styled.div`
 const Container = styled.div`
   box-shadow: 0 1px 10px rgba(0, 0, 0, 0.03);
   width: 100%;
-  max-height: 380px;
   background-color: #ffff;
   cursor: pointer;
 
   :hover {
-    box-shadow: 0 3px 15px rgba(0, 0, 0, 0.1);
-    font-weight: bold;
+    /* box-shadow: 0 3px 15px rgba(0, 0, 0, 0.1);
+    font-weight: bold; */
   }
-`;
-
-const ContainerImage = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: auto;
-  padding: 10px;
-`;
-
-const Img = styled.img`
-  width: auto;
-  height: auto;
-  padding: 10px;
 `;
 
 const Descricao = styled.div`
@@ -44,21 +28,10 @@ const Descricao = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
-  padding-bottom: 20px;
-`;
-
-const Preco = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  padding-bottom: 10px;
-  font-weight: bold;
 `;
 
 const Price = styled.p`
   color: #f38a00;
-  font-size: 16px;
 `;
 
 
@@ -83,17 +56,23 @@ const GridItem = (props) => {
   };
 
   return (
-    <Item className="column is-6-mobile is-4-tablet is-4-desktop">
-      <Container>
-        <ContainerImage>
-          {load ? (<Spinner />) : (<Img src={image} />)}
-        </ContainerImage>
-        <Preco>
-          <Price>{intl.formatNumber(item.valorVenda, { style: 'currency', currency: 'BRL' })}</Price>
-        </Preco>
-        <Descricao>
-          <p>{item.descricao}</p>
-        </Descricao>
+    <Item className="column is-12-mobile is-4-tablet is-4-desktop">
+      <Container className="card-image">
+        <div className="card-image">
+          <figure className="image is-160x160">
+            {load ? (<Spinner />) : (<img src={image} alt="product" />)}
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="title is-5">
+            <Price>
+              {intl.formatNumber(item.valorVenda, { style: 'currency', currency: 'BRL' })}
+            </Price>
+          </div>
+          <Descricao className="content">
+            <p>{item.descricao}</p>
+          </Descricao>
+        </div>
       </Container>
     </Item>
   );
