e4eb60a164d2eec8ff459d1310f8cb9a2971a103
Marcelo Sousa <marcelo@netpos.com.br>
2019-10-31 16:04:02 -0300

    Subheader catalog on mobile

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -26,6 +26,10 @@ const Container = styled.nav`
   }
 `;
 
+const AreaMenu = styled.div`
+  padding: 10px 0 10px 0;
+`;
+
 const Field = styled.div`
   justify-content: center;
   width: 80%;
@@ -82,7 +86,6 @@ const SearchButton = styled.button`
 `;
 
 const CartArea = styled.div`
-  padding-top: 23px;
 `;
 
 const CartIcon = styled.i`
@@ -90,6 +93,7 @@ const CartIcon = styled.i`
   position: relative;
   color: #fff;
   font-size: 1.5rem;
+  top: 9px;
 `;
 
 const CartCounter = styled.div`
@@ -150,9 +154,13 @@ const Header = (props) => {
     window.history.pushState({}, '', `${baseUrl}?search=${search}`);
   };
 
+  const { pathname } = history.location;
+
   return (
     <Container className="fixed-top">
-      <div className="container">
+      <AreaMenu
+        className="container"
+      >
         <Row>
           <Grid
             cols="2 3 3 3 3"
@@ -183,9 +191,12 @@ const Header = (props) => {
               </form>
             </Field>
           </Grid>
-          <Grid cols="2 2 2 2 1">
-            {(shop.is_enableOrder === 1) && (
-              <CartArea>
+          <Grid
+            className="d-flex align-items-center justify-content-center"
+            cols="2 2 2 2 1"
+          >
+            <CartArea>
+              {(shop.is_enableOrder === 1) && (
                 <CartIcon
                   onClick={() => {
                     history.push('/cart');
@@ -198,14 +209,16 @@ const Header = (props) => {
                     {shoppingCart.basketCount}
                   </CartCounter>
                 </CartIcon>
-              </CartArea>
-            )}
+              )}
+            </CartArea>
           </Grid>
         </Row>
-      </div>
-      <FiltersMobile
-        categories={categories}
-      />
+      </AreaMenu>
+      {(pathname === '/' && (
+        <FiltersMobile
+          categories={categories}
+        />
+      ))}
     </Container>
   );
 };
