ed5c622abf7983c1a3c20363324ca5b63492ab16
Marcelo Sousa <mns.marcelo67@gmail.com>
2019-11-14 16:36:45 -0300

    Typo

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -119,7 +119,7 @@ const Checkout = ({ intl }) => {
     createOrder(values).then((response) => {
       localStorage.removeItem('cartInit');
       localStorage.removeItem('cart');
-      const msg = `Você acabou de receber o pedido ${response.data.orderName} do seu catalogo online SmartPOS, acesse o app ou site e verifique nos pedidos em aberto.`;
+      const msg = `Você acabou de receber o pedido ${response.data.orderName} do seu catálogo online SmartPOS, acesse o app ou site e verifique nos pedidos em aberto.`;
       const linkWhatsApp = `<a href='https://api.whatsapp.com/send?phone=55${shop.whatsapp}&text=${encodeURIComponent(msg)}' target='blank'>Enviar confirmação do pedido por WhatsApp.</a>`;
       Swal.fire({
         type: 'success',
