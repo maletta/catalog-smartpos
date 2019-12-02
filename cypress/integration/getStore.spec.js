6b82be66e40315f79890d2e89fb95a71177e0672
Marcelo Sousa <marcelo@netpos.com.br>
2019-12-02 13:19:06 -0200

    Change url

--- cypress/integration/getStore.spec.js
+++ cypress/integration/getStore.spec.js
@@ -2,7 +2,7 @@ import store from './store_test';
 
 describe('Test Store', () => {
   it('check if smartposbr is renderer', () => {
-    cy.visit(`http://localhost:3000/${store.name}`);
+    cy.visit('https://cypress-test.qa.smartpos.net.br/');
     cy.get('.breadcrumb').contains(store.name);
     cy.title().should('eq', store.title);
   });
