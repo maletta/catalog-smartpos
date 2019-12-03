df9aebf923c42cf6a39e6f1cafd2c4afb404f589
Marcelo Sousa <mns.marcelo67@gmail.com>
2019-12-02 21:01:20 -0300

    Add 404 test

--- cypress/integration/getStore.spec.js
+++ cypress/integration/getStore.spec.js
@@ -1,10 +1,14 @@
 import store from './store_test';
 
 describe('Test Store', () => {
-  it.only('check if smartposbr is renderer', () => {
+  it('check if smartposbr is renderer', () => {
     cy.visit(`http://localhost:3000/${store.name}`);
     cy.request('cypress-test').its('body').should('contains', 'You need to enable JavaScript to run this app.');
     // cy.get('.breadcrumb').contains(store.name);
     // cy.title().should('eq', store.title);
+  });
+  it('Test 404 page', () => {
+    cy.visit('http://localhost:3000/erro-404-sdsd');
+    cy.get('.404page').contains('Loja não encontrada.');
   });
 });
