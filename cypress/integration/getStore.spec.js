a5131be359652dce0387c11e3f5d4e4e7a27fcc3
Marcelo Sousa <mns.marcelo67@gmail.com>
2019-12-03 20:41:28 -0300

    Remove tests

--- cypress/integration/getStore.spec.js
+++ cypress/integration/getStore.spec.js
@@ -4,11 +4,11 @@ describe('Test Store', () => {
   it('check if smartposbr is renderer', () => {
     cy.visit(`http://localhost:3000/${store.name}`);
     cy.request('cypress-test').its('body').should('contains', 'You need to enable JavaScript to run this app.');
-    // cy.get('.breadcrumb').contains(store.name);
-    // cy.title().should('eq', store.title);
+    cy.get('.breadcrumb').contains(store.name);
+    cy.title().should('eq', store.title);
   });
-  it('Test 404 page', () => {
-    cy.visit('http://localhost:3000/cypress-test');
-    cy.get('.404page').contains('Loja não encontrada.');
-  });
+  // it('Test 404 page', () => {
+  //   cy.visit('http://localhost:3000/cypress-test');
+  //   cy.get('.404page').contains('Loja não encontrada.');
+  // });
 });
