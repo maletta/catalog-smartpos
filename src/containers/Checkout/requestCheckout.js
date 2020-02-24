a2d3875e991935bf3b0d7c9e10927b9c53b6471e
Marcelo Sousa <mns.marcelo67@gmail.com>
2020-02-24 08:49:35 -0300

    Add select payment type

--- src/containers/Checkout/requestCheckout.js
+++ src/containers/Checkout/requestCheckout.js
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 const createOrder = values => (axios.post(`${process.env.REACT_APP_MAIN_API}/v1/loja/${values.catalog_id}/pedido`, values));
 
-export default createOrder;
+const getPayments = storeID => (axios.get(`${process.env.REACT_APP_MAIN_API}/v1/loja/${storeID}/pagamentos`));
+
+export {
+  createOrder as default,
+  getPayments,
+};
