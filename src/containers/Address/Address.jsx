7c275b7b242174985987b2fd4137e71548c38520
Douglas Elias <douglas.elias@netpos.com.br>
2020-06-23 16:29:37 -0300

    Add cep request

--- src/containers/Address/Address.jsx
+++ src/containers/Address/Address.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Formik, Form, Field } from 'formik';
 import axios from 'axios';
@@ -13,6 +13,7 @@ import MaskedNumberInput from 'components/Form/MaskedNumberInput';
 import Button from 'components/Form/Button';
 import SelectDropDown from 'components/Form/SelectDropDown';
 import PurchasePrices from 'containers/Cart/components/PurchasePrices';
+import ShoppingCartContext from 'contexts/ShoppingCartContext';
 
 import addressSchema from './addressSchema';
 
@@ -38,79 +39,115 @@ const StepsContainer = styled.div`
   width: 100%;
 `;
 
-const RegisterData = () => (
-  <Container className="row">
-    <Grid cols="12 12 12 8 8" className="pt-3">
-      <StepsContainer>
-        <Steps activeIndex={2} />
-      </StepsContainer>
-      <Formik
-        onSubmit={() => {
-          console.log('submit');
-          return;
-          history.push('/payment');
-        }}
-        initialValues={{
-          cep: '',
-          endereco: '',
-          tipoLogradouro: '',
-          tipoEndereco: addressType[0],
-          complemento: '',
-          numero: '',
-          bairro: '',
-          cidade: '',
-          codcidade: '',
-          estado: '',
-        }}
-        validationSchema={addressSchema()}
-        render={propsForm => (
-          <Form>
-            <Row>
-              <Grid cols="12">
-                <SectionTitle>Endereço da entrega</SectionTitle>
-              </Grid>
-              <Grid cols="12 6 6 4 4">
-                <Field
-                  label="CEP"
-                  name="cep"
-                  inputId="cep"
-                  type="tel"
-                  format="#####-###"
-                  component={MaskedNumberInput}
-                  onValueChange={(values) => {
-                    if (values.value.length < 8) return;
+const RegisterData = () => {
+  const { shoppingCart, updateShoppingCart } = useContext(ShoppingCartContext);
+  const [address, setAddress] = useState({
+    cep: '',
+    endereco: '',
+    tipoLogradouro: '',
+    tipoEndereco: addressType[0],
+    complemento: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    codcidade: '',
+    estado: '',
+  });
 
-                    propsForm.setFieldValue('cep', values.formattedValue);
-                    getCep(values.value).then((address) => {
-                      if (address.data.logradouro) {
-                        const tipoLogradouro = address.data.logradouro.substring(
-                          0,
-                          address.data.logradouro.indexOf(' ') + 1,
-                        );
-                        const endereco = address.data.logradouro.substring(
-                          address.data.logradouro.indexOf(' ') + 1,
-                        );
-                        propsForm.setFieldValue('endereco', endereco.trim());
+  console.log('start-sp');
+  console.log(shoppingCart);
+  console.log('end-sp');
+
+  // data:
+  // bairro: "Cidade Antônio Estevão de Carvalho"
+  // cep: "08226-021"
+  // complemento: ""
+  // gia: "1004"
+  // ibge: "3550308"
+  // localidade: "São Paulo"
+  // logradouro: "Rua 18 de Abril"
+  // uf: "SP"
+  // unidade: ""
+
+  useEffect(() => {
+    console.log('use effect');
+    if (shoppingCart.cep.length > 0) {
+      getCep(shoppingCart.cep).then((({ data }) => {
+        const addressData = {
+          cep: shoppingCart.cep,
+          endereco: data.logradouro,
+          tipoLogradouro: data.logradouro.split(' ')[0],
+          bairro: data.bairro,
+          cidade: data.localidade,
+          codcidade: data.ibge,
+          estado: data.uf,
+        };
+        console.log(addressData);
+        setAddress(addressData);
+      }));
+      getCep(shoppingCart.cep).then((console.log));
+    }
+  }, []);
+
+  return (
+    <Container className="row">
+      <Grid cols="12 12 12 8 8" className="pt-3">
+        <StepsContainer>
+          <Steps activeIndex={2} />
+        </StepsContainer>
+        <Formik
+          enableReinitialize
+          onSubmit={() => {
+            history.push('/payment');
+          }}
+          initialValues={address}
+          validationSchema={addressSchema()}
+          render={propsForm => (
+            <Form>
+              <Row>
+                <Grid cols="12">
+                  <SectionTitle>Endereço da entrega</SectionTitle>
+                </Grid>
+                <Grid cols="12 6 6 4 4">
+                  <Field
+                    label="CEP"
+                    name="cep"
+                    inputId="cep"
+                    type="tel"
+                    format="#####-###"
+                    component={MaskedNumberInput}
+                    onValueChange={(values) => {
+                      if (values.value.length < 8) return;
+
+                      propsForm.setFieldValue('cep', values.formattedValue);
+                      getCep(values.value).then((address) => {
+                        if (address.data.logradouro) {
+                          const tipoLogradouro = address.data.logradouro.substring(
+                            0,
+                            address.data.logradouro.indexOf(' ') + 1,
+                          );
+                          const endereco = address.data.logradouro.substring(
+                            address.data.logradouro.indexOf(' ') + 1,
+                          );
+                          propsForm.setFieldValue('endereco', endereco.trim());
+                          propsForm.setFieldValue(
+                            'tipoLogradouro',
+                            tipoLogradouro.trim(),
+                          );
+                        }
+                        propsForm.setFieldValue('bairro', address.data.bairro);
+                        propsForm.setFieldValue('estado', address.data.uf);
+                        propsForm.setFieldValue('codcidade', address.data.ibge);
                         propsForm.setFieldValue(
-                          'tipoLogradouro',
-                          tipoLogradouro.trim(),
+                          'cidade',
+                          address.data.localidade,
                         );
-                      }
-                      propsForm.setFieldValue('bairro', address.data.bairro);
-                      propsForm.setFieldValue('estado', address.data.uf);
-                      propsForm.setFieldValue('codcidade', address.data.ibge);
-                      propsForm.setFieldValue(
-                        'cidade',
-                        address.data.localidade,
-                      );
-                    });
-                    propsForm.setFieldValue('installments', null);
-                    // costDeliveryApi(values.formattedValue, propsForm);
-                  }}
-                  isRequired
-                />
-              </Grid>
-              {/* <Grid cols="12 6 6 3 3">
+                      });
+                    }}
+                    isRequired
+                  />
+                </Grid>
+                {/* <Grid cols="12 6 6 3 3">
                   <Field
                     label="Tipo logradouro"
                     name="tipoLogradouro"
@@ -120,98 +157,100 @@ const RegisterData = () => (
                     isRequired
                   />
                 </Grid> */}
-              <Grid cols="12 6 6 8 8">
-                <Field
-                  label="Endereço"
-                  name="endereco"
-                  inputId="endereco"
-                  component={Input}
-                  isRequired
-                />
-              </Grid>
-              <Grid cols="12 6 6 6 4">
-                <Field
-                  label="Número"
-                  name="numero"
-                  inputId="numero"
-                  component={Input}
-                  isRequired
-                  type="tel"
-                />
-              </Grid>
-              <Grid cols="12 6 6 6 8">
-                <Field
-                  label="Complemento"
-                  name="complemento"
-                  inputId="complemento"
-                  component={Input}
-                />
-              </Grid>
-              <Grid cols="12 12 8 8 8">
-                <Field
-                  label="Bairro"
-                  name="bairro"
-                  inputId="bairro"
-                  component={Input}
-                  isRequired
-                />
-              </Grid>
-              <Grid cols="12 12 4 4 4">
-                <SelectDropDown
-                  id="tipoEndereco"
-                  label="Tipo de endereço"
-                  cacheOptions
-                  options={addressType}
-                  defaultValue={propsForm.values.tipoEndereco}
-                  getOptionLabel={label => label.label}
-                  getOptionValue={option => option.value}
-                  onChange={(event) => {
-                    propsForm.setFieldValue('tipoEndereco', event);
-                  }}
-                  isInvalid={propsForm.errors.tipoEndereco}
-                  touched={propsForm.touched.tipoEndereco}
-                />
-              </Grid>
-              <Grid cols="12 6 6 6 6">
-                <Field
-                  label="Cidade"
-                  name="cidade"
-                  inputId="cidade"
-                  component={Input}
-                  isRequired
-                  disabled
-                />
-              </Grid>
-              <Grid cols="12 6 6 6 6">
-                <Field
-                  label="Estado"
-                  name="estado"
-                  inputId="estado"
-                  component={Input}
-                  isRequired
-                  disabled
-                />
-              </Grid>
-            </Row>
-            <Row className="d-flex justify-content-end pb-4 pr-3">
-              <Button value="Próximo" type="submit" />
-            </Row>
-          </Form>
-        )}
-      />
-    </Grid>
-    <Grid cols="12 12 12 4 4" style={{ padding: 0 }}>
-      <PurchasePrices
-        // basketCountCart={basketCountCart}
-        // totalCart={totalCart}
-        // deliveryCost={deliveryCost}
-        basketCountCart={0}
-        totalCart={0}
-        deliveryCost={{}}
-        couponValue={-5}
-      />
-    </Grid>
-  </Container>
-);
+                <Grid cols="12 6 6 8 8">
+                  <Field
+                    label="Endereço"
+                    name="endereco"
+                    inputId="endereco"
+                    component={Input}
+                    isRequired
+                  />
+                </Grid>
+                <Grid cols="12 6 6 6 4">
+                  <Field
+                    label="Número"
+                    name="numero"
+                    inputId="numero"
+                    component={Input}
+                    isRequired
+                    type="tel"
+                  />
+                </Grid>
+                <Grid cols="12 6 6 6 8">
+                  <Field
+                    label="Complemento"
+                    name="complemento"
+                    inputId="complemento"
+                    component={Input}
+                  />
+                </Grid>
+                <Grid cols="12 12 8 8 8">
+                  <Field
+                    label="Bairro"
+                    name="bairro"
+                    inputId="bairro"
+                    component={Input}
+                    isRequired
+                  />
+                </Grid>
+                <Grid cols="12 12 4 4 4">
+                  <SelectDropDown
+                    id="tipoEndereco"
+                    label="Tipo de endereço"
+                    cacheOptions
+                    options={addressType}
+                    defaultValue={propsForm.values.tipoEndereco}
+                    getOptionLabel={label => label.label}
+                    getOptionValue={option => option.value}
+                    onChange={(event) => {
+                      propsForm.setFieldValue('tipoEndereco', event);
+                    }}
+                    isInvalid={propsForm.errors.tipoEndereco}
+                    touched={propsForm.touched.tipoEndereco}
+                  />
+                </Grid>
+                <Grid cols="12 6 6 6 6">
+                  <Field
+                    label="Cidade"
+                    name="cidade"
+                    inputId="cidade"
+                    component={Input}
+                    isRequired
+                    disabled
+                  />
+                </Grid>
+                <Grid cols="12 6 6 6 6">
+                  <Field
+                    label="Estado"
+                    name="estado"
+                    inputId="estado"
+                    component={Input}
+                    isRequired
+                    disabled
+                  />
+                </Grid>
+              </Row>
+              <Row className="d-flex justify-content-end pb-4 pr-3">
+                <Button value="Próximo" type="submit" />
+              </Row>
+            </Form>
+          )}
+        />
+      </Grid>
+      <Grid cols="12 12 12 4 4" style={{ padding: 0 }}>
+        <PurchasePrices
+          // basketCountCart={basketCountCart}
+          // totalCart={totalCart}
+          // deliveryCost={deliveryCost}
+          basketCountCart={shoppingCart.basketCount}
+          totalCart={shoppingCart.cart.reduce((count, val) => count + val.quantity * (val.pricing.modifiers + val.pricing.product),
+            0)}
+          deliveryCost={{}}
+          couponValue={-5}
+        />
+      </Grid>
+    </Container>
+  );
+};
 
 export default RegisterData;
