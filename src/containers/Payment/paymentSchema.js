b25822d9490751dc3b108afbfb382787400db515
Douglas Elias <douglas.elias@netpos.com.br>
2020-07-03 12:02:47 -0300

    Adiciona mensagens de erro na tela de pagamento

--- src/containers/Payment/paymentSchema.js
+++ src/containers/Payment/paymentSchema.js
@@ -6,7 +6,7 @@ import validateCpf from 'utils/validation/validateCpf';
 const paymentSchema = yup.object().shape({
   nameHolder: yup
     .string()
-    .required()
+    .required('Nome é obrigatório')
     .test('nameHolder', 'Nome do titular', (value) => {
       if (!value) return false;
 
@@ -15,20 +15,23 @@ const paymentSchema = yup.object().shape({
     }),
   cpfHolder: yup
     .string()
+    .required('CPF é obrigatório')
     .test('cpf-valid', 'CPF inválido', value => validateCpf(value)),
   birthDateHolder: yup
     .string()
+    .required('Data de nascimento é obrigatória')
     .test('birthDateHolder', 'Campo inválido', inputValue => moment(inputValue, 'DD/MM/YYYY', true).isValid()),
   cardNumber_unformatted: yup
     .string()
-    .required()
+    .required('Número do cartão é obrigatório')
     .min(14, 'Inválido'),
   expiration: yup
     .string()
+    .required('Validade é obrigatória')
     .test('expiration', 'Campo inválido', inputValue => moment(inputValue, 'MM/YYYY', true).isValid()),
   cvv: yup
     .string()
-    .required()
+    .required('Código de segurança é obrigatório')
     .min(3)
     .max(4),
   installments: yup.object().required(),
