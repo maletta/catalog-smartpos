0ef77b3f68e2cbd8b132c31591452db8de9da8ff
Douglas Elias <douglas.elias@netpos.com.br>
2020-07-23 19:04:21 -0300

    refactor: manifesto dinamico

--- src/utils/getStoreName.js
+++ src/utils/getStoreName.js
@@ -2,7 +2,7 @@ export const hasDomainName = process.env.REACT_APP_GET_NAME_DOMAIN === 'true';
 export const getSubDomain = url => url.hostname.split('.')[0];
 export const getStoreNameFromURL = url => url.pathname.split('/')[1];
 
-export const getStoreName = () => {
+const getStoreName = () => {
   const fn = hasDomainName ? getSubDomain : getStoreNameFromURL;
   return fn(window.location);
 };
