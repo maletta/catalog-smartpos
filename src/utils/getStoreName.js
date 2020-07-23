59418473d4ec9714aa48ed193bb5b41daf80b398
Douglas Elias <douglas.elias@netpos.com.br>
2020-07-23 18:19:37 -0300

    feat: adiciona teste para store name

--- src/utils/getStoreName.js
+++ src/utils/getStoreName.js
@@ -1,20 +1,15 @@
-const getStoreName = () => {
-  if (process.env.REACT_APP_GET_NAME_DOMAIN === 'true') {
-    const parts = window.location.hostname.split('.');
-    const subdomain = parts.shift();
-    return subdomain;
-  }
-  return window.location.pathname.split('/')[1];
+export const hasDomainName = process.env.REACT_APP_GET_NAME_DOMAIN === 'true';
+export const getSubDomain = url => url.hostname.split('.')[0];
+export const getStoreNameFromURL = url => url.pathname.split('/')[1];
+
+export const getStoreName = () => {
+  const fn = hasDomainName ? getSubDomain : getStoreNameFromURL;
+  return fn(window.location);
 };
 
-const getBaseName = () => {
-  if (process.env.REACT_APP_GET_NAME_DOMAIN === 'true') {
-    return '/';
-  }
-  return `/${getStoreName()}`;
+export const getBaseName = () => {
+  const result = hasDomainName ? '' : getStoreNameFromURL(window.location);
+  return `/${result}`;
 };
 
-export {
-  getStoreName as default,
-  getBaseName,
-};
+export default getStoreName;
