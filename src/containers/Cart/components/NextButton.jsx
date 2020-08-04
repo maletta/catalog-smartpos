2f278f17561389277fe50fecdb70d0c61e6ea5ec
Douglas Elias <douglas.elias@netpos.com.br>
2020-08-04 12:24:47 -0300

    fix: erros do eslint

--- src/containers/Cart/components/NextButton.jsx
+++ src/containers/Cart/components/NextButton.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Button from 'components/Form/Button';
 import ShopContext from 'contexts/ShopContext';
