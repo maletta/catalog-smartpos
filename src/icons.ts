b37b35aca5ef9837caa4d58e93c9e3ae6c3bf35b
Douglas Elias <douglas.elias@netpos.com.br>
2020-08-10 12:20:40 -0300

    refactor: extrai icones para arquivo separado

---
+++ src/icons.ts
@@ -0,0 +1,42 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCheck,
+  faList,
+  faTh,
+  faMapMarkerAlt,
+  faPhone,
+  faEnvelope,
+  faArrowRight,
+  faArrowLeft,
+  faCaretDown,
+  faSlidersH,
+  faSort,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookF,
+  faWhatsapp,
+  faInstagram,
+  faGooglePlay,
+} from '@fortawesome/free-brands-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+
+library.add(
+  faCheck,
+  faList,
+  faTh,
+  faMapMarkerAlt,
+  faPhone,
+  faEnvelope,
+  faFacebookF,
+  faTimes,
+  faGooglePlay,
+  faWhatsapp,
+  faInstagram,
+  faHeart,
+  faArrowRight,
+  faArrowLeft,
+  faCaretDown,
+  faSlidersH,
+  faSort,
+);
