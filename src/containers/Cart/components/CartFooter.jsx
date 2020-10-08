bf189b22de18bc163ceec35247e7362dc9b514af
Alexandre Ribeiro da Silva <alexandre.ribeiro@netpos.com.br>
2020-10-08 11:22:26 -0300

    feat: escondendo o campo para aplicar cupom de desconto

--- src/containers/Cart/components/CartFooter.jsx
+++ src/containers/Cart/components/CartFooter.jsx
@@ -1,4 +1,8 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, {
+  useContext,
+  // useState,
+  // useEffect,
+} from 'react';
 
 import Grid from 'components/Grid';
 import Row from 'components/Row';
@@ -10,30 +14,30 @@ import { DeliveryContainer } from './cartFooterStyled';
 import DeliveryCEPInput from './DeliveryCEPInput';
 import AddMoreItemsButton from './AddMoreItemsButton';
 import NextButton from './NextButton';
-import CouponContainer from './CouponContainer';
-import { checkingCouponsAvailable } from './cartFooterRequest';
+// import CouponContainer from './CouponContainer';
+// import { checkingCouponsAvailable } from './cartFooterRequest';
 
 const CartFooter = () => {
   const { shoppingCart, updateShoppingCart } = useContext(ShoppingCartContext);
   const { shop } = useContext(ShopContext);
 
-  const [couponsAvailable, setCouponsAvailable] = useState(false);
+  // const [couponsAvailable, setCouponsAvailable] = useState(false);
 
   const handleChangeDelivery = ({ target }) => {
     updateShoppingCart({ withdraw: target.value === 'PICKUP' });
   };
 
-  useEffect(() => {
-    checkingCouponsAvailable(shop.id).then((response) => {
-      const { available } = response.data;
+  // useEffect(() => {
+  //   checkingCouponsAvailable(shop.id).then((response) => {
+  //     const { available } = response.data;
 
-      if (available) {
-        setCouponsAvailable(available);
-      }
-    }).catch(() => {
-      setCouponsAvailable(false);
-    });
-  }, [shop.id]);
+  //     if (available) {
+  //       setCouponsAvailable(available);
+  //     }
+  //   }).catch(() => {
+  //     setCouponsAvailable(false);
+  //   });
+  // }, [shop.id]);
 
   return (
     <>
@@ -62,12 +66,12 @@ const CartFooter = () => {
             {!shoppingCart.withdraw && <DeliveryCEPInput />}
           </DeliveryContainer>
         </Grid>
-        {couponsAvailable && (
+        {/* {couponsAvailable && (
           <Grid cols="12 6 6 6 6">
             <p>Cupom de desconto:</p>
             <CouponContainer />
           </Grid>
-        )}
+        )} */}
       </Row>
       <Row>
         <Grid cols="12" className="d-flex justify-content-end mb-4">
