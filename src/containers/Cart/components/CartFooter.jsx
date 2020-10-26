fb00d30aa75b02f04283c5133cf1cad4860fa819
Alexandre Ribeiro da Silva <alexandre.ribeiro@netpos.com.br>
2020-10-26 19:21:50 -0300

    feat: descomitando o codigo do cupom de desconto

--- src/containers/Cart/components/CartFooter.jsx
+++ src/containers/Cart/components/CartFooter.jsx
@@ -1,7 +1,7 @@
 import React, {
   useContext,
-  // useState,
-  // useEffect,
+  useState,
+  useEffect,
 } from 'react';
 
 import Grid from 'components/Grid';
@@ -14,30 +14,30 @@ import { DeliveryContainer } from './cartFooterStyled';
 import DeliveryCEPInput from './DeliveryCEPInput';
 import AddMoreItemsButton from './AddMoreItemsButton';
 import NextButton from './NextButton';
-// import CouponContainer from './CouponContainer';
-// import { checkingCouponsAvailable } from './cartFooterRequest';
+import CouponContainer from './CouponContainer';
+import { checkingCouponsAvailable } from './cartFooterRequest';
 
 const CartFooter = () => {
   const { shoppingCart, updateShoppingCart } = useContext(ShoppingCartContext);
   const { shop } = useContext(ShopContext);
 
-  // const [couponsAvailable, setCouponsAvailable] = useState(false);
+  const [couponsAvailable, setCouponsAvailable] = useState(false);
 
   const handleChangeDelivery = ({ target }) => {
     updateShoppingCart({ withdraw: target.value === 'PICKUP' });
   };
 
-  // useEffect(() => {
-  //   checkingCouponsAvailable(shop.id).then((response) => {
-  //     const { available } = response.data;
+  useEffect(() => {
+    checkingCouponsAvailable(shop.id).then((response) => {
+      const { available } = response.data;
 
-  //     if (available) {
-  //       setCouponsAvailable(available);
-  //     }
-  //   }).catch(() => {
-  //     setCouponsAvailable(false);
-  //   });
-  // }, [shop.id]);
+      if (available) {
+        setCouponsAvailable(available);
+      }
+    }).catch(() => {
+      setCouponsAvailable(false);
+    });
+  }, [shop.id]);
 
   return (
     <>
@@ -66,12 +66,12 @@ const CartFooter = () => {
             {!shoppingCart.withdraw && <DeliveryCEPInput />}
           </DeliveryContainer>
         </Grid>
-        {/* {couponsAvailable && (
+        {couponsAvailable && (
           <Grid cols="12 6 6 6 6">
             <p>Cupom de desconto:</p>
             <CouponContainer />
           </Grid>
-        )} */}
+        )}
       </Row>
       <Row>
         <Grid cols="12" className="d-flex justify-content-end mb-4">
